626057d9d6aab661e8fb9eb53b545283629f780f
Anshul <ay477510@gmail.com>
2026-05-16 04:47:56 +0000

    Adder Minter , Burner roles and tests

--- src/StableCoin.sol
+++ src/StableCoin.sol
@@ -1,26 +1,62 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+/// @title StableCoin
+/// @author Anshul
+/// @notice ERC20 based stablecoin for the protocol
+/// @dev Minting and burning controlled by owner/treasury
+
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-contract StableCoin is ERC20, Ownable {
-    address public s_treasury;
+import {
+    ERC20Burnable
+} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 
-    error Not_Treasury();
+contract StableCoin is ERC20, ERC20Burnable, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
 
-    constructor(address treasury) ERC20("StableCoin", "STC") Ownable(msg.sender) {
-        s_treasury = treasury;
+    error StableCoin__NotMinter();
+    error StableCoin__ZeroAddress();
+    error StableCoin__InsufficientBalance(uint256 balance, uint256 amount);
+    error StableCoin__ZeroAmount();
+
+    event MinterRoleGranted(address indexed account);
+    event BurnerRoleGranted(address indexed account);
+
+    /**
+     * @param admin  Address receiving DEFAULT_ADMIN_ROLE.
+     *               In production: a multisig (Gnosis Safe).
+     *               In tests: the deployer.
+     */
+
+    constructor(address admin) ERC20("UbiQuin", "UBQ") {
+        if (admin == address(0)) revert StableCoin__ZeroAddress();
+        _grantRole(DEFAULT_ADMIN_ROLE, admin);
     }
 
-    
-    function mint(address to, uint256 amount) external {
-        if (msg.sender != s_treasury) {
-            revert Not_Treasury();
-        }
+    function mint(address to, uint256 amount) external onlyRole(MINTER_ROLE) {
+        if (to == address(0)) revert StableCoin__ZeroAddress();
+        if (amount == 0) revert StableCoin__ZeroAmount();
         _mint(to, amount);
     }
 
-    function burn(uint256 amount) external {
-        _burn(msg.sender, amount);
+    function burnFrom(address from , uint256 amount) public override onlyRole(BURNER_ROLE){
+        if(from == address(0)) revert StableCoin__ZeroAddress();
+        if(amount == 0) revert StableCoin__ZeroAmount();
+        if(amount > balanceOf(from)) revert StableCoin__InsufficientBalance(balanceOf(from), amount);
+        _burn(from, amount);    
+    }
+
+    function grantMinterRole(address account) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        if (account == address(0)) revert StableCoin__ZeroAddress();
+        _grantRole(MINTER_ROLE, account);
+        emit MinterRoleGranted(account);
+    }
+
+    function grantBurnerRole(address account) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        if (account == address(0)) revert StableCoin__ZeroAddress();
+        grantRole(BURNER_ROLE, account);
+        emit BurnerRoleGranted(account);
     }
 }
--- test/StableCoin.test.t.sol
+++ test/StableCoin.test.t.sol
@@ -6,24 +6,121 @@ import {StableCoin} from "../src/StableCoin.sol";
 
 contract StableCoinTest is Test {
     StableCoin public stableCoin;
-    address public treasury = address(0x3);
+    /**
+     * @dev Address of the minter and burner in tests.
+      In production, these would be the treasury or a multisig (Gnosis Safe).
+     */
+    address public constant MINTER = 0x15d34AAf54267DB7D7c367839AAf71A00a2C6A65;
+    address public constant BURNER = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8;
     function setUp() public {
-        stableCoin = new StableCoin(treasury);
-    }
-    function testTreasurySetCorrectly() public {
-        assertEq(stableCoin.s_treasury(), treasury);
+        stableCoin = new StableCoin(address(this));
     }
     function testMint() public {
-        vm.expectRevert();
-        stableCoin.mint(address(this), 100);
+        stableCoin.grantMinterRole(MINTER);
+        vm.prank(MINTER);
+        stableCoin.mint(address(this), 10);
+        assertEq(stableCoin.balanceOf(address(this)), 10);
     }
+
     function testBurn() public {
-        vm.prank(treasury);
-        stableCoin.mint(address(this), 100);
-        stableCoin.burn(50);
-        assertEq(
-            stableCoin.balanceOf(address(this)),
-            50
-        );
+        stableCoin.grantMinterRole(MINTER);
+        stableCoin.grantBurnerRole(BURNER);
+        vm.prank(MINTER);
+        stableCoin.mint(address(this), 10);
+        vm.prank(BURNER);
+        stableCoin.burnFrom(address(this), 5);
+        assertEq(stableCoin.balanceOf(address(this)), 5);
+    }
+
+    function testInsufficientBalanceBurn() public {
+        stableCoin.grantMinterRole(MINTER);
+        stableCoin.grantBurnerRole(BURNER);
+        vm.prank(MINTER);
+        stableCoin.mint(address(this), 10);
+        vm.prank(BURNER);
+        vm.expectRevert();
+        stableCoin.burnFrom(address(this), 20);
+    }
+
+    function testNonMinterCannotMint() public {
+        vm.expectRevert();
+        stableCoin.mint(address(this), 10);
+    }
+
+    function testNonBurnerCannotBurn() public {
+        stableCoin.grantMinterRole(MINTER);
+        vm.prank(MINTER);
+        stableCoin.mint(address(this), 10);
+        vm.expectRevert();
+        stableCoin.burnFrom(address(this), 5);
+    }
+
+    function testCannotMintToZeroAddress() public {
+        stableCoin.grantMinterRole(MINTER);
+        vm.prank(MINTER);
+        vm.expectRevert(StableCoin.StableCoin__ZeroAddress.selector);
+        stableCoin.mint(address(0), 10);
+    }
+
+    function testCannotMintZeroAmount() public {
+        stableCoin.grantMinterRole(MINTER);
+        vm.prank(MINTER);
+        vm.expectRevert(StableCoin.StableCoin__ZeroAmount.selector);
+        stableCoin.mint(address(this), 0);
+    }
+
+    function testCannotBurnZeroAmount() public {
+        stableCoin.grantBurnerRole(BURNER);
+        vm.prank(BURNER);
+        vm.expectRevert(StableCoin.StableCoin__ZeroAmount.selector);
+        stableCoin.burnFrom(address(this), 0);
+    }
+
+    function testBurnReducesTotalSupply() public {
+        stableCoin.grantMinterRole(MINTER);
+        stableCoin.grantBurnerRole(BURNER);
+        vm.prank(MINTER);
+        stableCoin.mint(address(this), 10);
+        vm.prank(BURNER);
+        stableCoin.burnFrom(address(this), 5);
+        assertEq(stableCoin.totalSupply(), 5);
+    }
+
+    function testNonAdminCannotGrant() public {
+        vm.prank(address(0));
+        vm.expectRevert();
+        stableCoin.grantMinterRole(MINTER);
+    }
+
+    function testMintIncreasesSupply() public {
+        stableCoin.grantMinterRole(MINTER);
+        vm.prank(MINTER);
+        stableCoin.mint(address(this), 10);
+        assertEq(stableCoin.totalSupply(), 10);
+    }
+
+    function testConstructorRejectsZeroAddress() public {
+        vm.expectRevert(StableCoin.StableCoin__ZeroAddress.selector);
+        new StableCoin(address(0));
+    }
+
+    function testCannotBurnFromZeroAddress() public {
+        stableCoin.grantBurnerRole(BURNER);
+        vm.prank(BURNER);
+        vm.expectRevert(StableCoin.StableCoin__ZeroAddress.selector);
+
+        stableCoin.burnFrom(address(0), 100);
+    }
+
+    function testCannotGrantMinterRoleToZeroAddress() public {
+        vm.expectRevert(StableCoin.StableCoin__ZeroAddress.selector);
+
+        stableCoin.grantMinterRole(address(0));
+    }
+
+    function testCannotGrantBurnerRoleToZeroAddress() public {
+        vm.expectRevert(StableCoin.StableCoin__ZeroAddress.selector);
+
+        stableCoin.grantBurnerRole(address(0));
     }
 }
